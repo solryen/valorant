e2750d69729bd1910124c3517a2866ec61f39e08
solryen <athymaritg@coventry.ac.uk>
2026-06-13 21:28:18 +0300

    Delete api directory

--- api/geo.js
+++
@@ -1,8 +0,0 @@
-export default function handler(req, res) {
-  const country = String(req.headers['x-vercel-ip-country'] ?? '').toUpperCase()
-  const language = country === 'GR' ? 'el' : 'en'
-
-  res.setHeader('Content-Type', 'application/json')
-  res.setHeader('Cache-Control', 'no-store')
-  res.status(200).json({ country, language })
-}
